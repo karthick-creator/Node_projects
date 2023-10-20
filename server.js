f8d46833b9d41f14e8fa20422207ae679ff99a10
Karthick G <111891234+karthickincode@users.noreply.github.com>
2023-10-20 10:11:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const {add,sub,mul,div} = require('./math')
+
+console.log(add(343,678))
+console.log(sub(23,6))
+console.log(mul(34,456))
+console.log(div(67,21))
+console.log("welcome to node js");
